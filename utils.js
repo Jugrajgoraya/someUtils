c489333740572b5dd3bbdea9dacc1ee675df3b4f
jugraj <jugraj98123@gmail.com>
2022-01-27 14:41:48 -0800

    power function added

--- utils.js
+++ utils.js
@@ -5,3 +5,11 @@ function product(a,b) {
 function devide(a,b){
     return a/b;
 }
+
+function power(a,b){
+    let result = 1;
+    for(let i = 0; i < b; i++){
+        result = result*a;
+    }
+    console.log(result);
+}
