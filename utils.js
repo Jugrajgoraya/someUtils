4b9577d3e3d8216d33182b9e07fca5f0987a48e0
Ibrahim <ibrahimood1@gmail.com>
2022-01-28 14:17:19 -0800

    utils.js

--- utils.js
+++ utils.js
@@ -1,19 +1,25 @@
-function product(a,b) {
-    return a*b;
-}
+// function product(a,b) {
+//     return a*b;
+// }
 
-function devide(a,b){
-    return a/b;
-}
+// function devide(a,b){
+//     return a/b;
+// }
 
-function power(a,b){
-    let result = 1;
-    for(let i = 0; i < b; i++){
-        result = result*a;
-    }
-    console.log(result);
-}
+// function power(a,b){
+//     let result = 1;
+//     for(let i = 0; i < b; i++){
+//         result = result*a;
+//     }
+//     console.log(result);
+// }
 
-function substract(a,b) {
-    return a-b;
+// function substract(a,b) {
+//     return a-b;
+// }
+
+
+function sum(a,b) {
+    return a + b
 }
+console.log(sum(3,4))
